e6934d9e53dbea2af3d184fef5a1f57b31becbda
EdKazik <edoskazik@gmail.com>
2023-05-30 14:25:26 +0200

    first commit task10

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let newBlock = document.createElement('div');
 newBlock.className = "new-block";
 newBlock.style.background = 'red';
@@ -6,6 +5,7 @@ newBlock.style.height = '30px';
 newBlock.style.width = '200px';
 const input = document.querySelector(".input");
 const cont = document.querySelector(".container");
+
 input.addEventListener("mouseover", () => {
     document.querySelector(".container").appendChild(newBlock);
     
